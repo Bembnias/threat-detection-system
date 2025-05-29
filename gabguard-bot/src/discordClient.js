41224a373f8a3d8daf90aa58554e62dc34ee4f28
Bembnias <mateusz.bienias@rad4m.com>
2025-05-29 21:57:10 +0200

    Instalacja zależności i utworzenie clienta (bota) discord

---
+++ gabguard-bot/src/discordClient.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.User, Partials.GuildMember],
+})
+
+module.exports = client
